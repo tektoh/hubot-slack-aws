c490515367369d824f53697c7465ff0e8f201f1f
Shinichi Segawa <tektoh@gmail.com>
2015-01-22 14:57:40 +0900

    delete vagrant

--- vagrant/provision-install.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get -y update
-sudo apt-get -y install redis-server redis-tools nodejs
-/usr/local/bin/node
--- vagrant/provision-start.sh
+++
@@ -1,7 +0,0 @@
-cd /vagrant
-if [ -f tmp/pids/server.pid ]; then
-  kill -KILL `cat tmp/pids/server.pid`
-  rm -f tmp/pids/server.pid
-fi
-bundle install
-bundle exec rails server -d -p 3000 -b 0.0.0.0 -e development
